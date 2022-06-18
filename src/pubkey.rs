92e1500cf40c6fb6aabae10533ebf6aaca5f0be3
David O'Rourke <david.orourke@gmail.com>
2022-06-18 23:53:58 +0100

    pubkey: Add a test to ensure we get the same output as wg

--- src/pubkey.rs
+++ src/pubkey.rs
@@ -11,9 +11,22 @@ use super::WG_KEY_LEN;
 
 const WG_KEY_LEN_BASE64: usize = (((WG_KEY_LEN + 2) / 3) * 4) + 1;
 
+// Generates a public key from a given private key.
+fn generate_public_key(b64: &str) -> Result<String> {
+    // Get the private key base64 and decode it
+    let private_key = Base64::decode_vec(b64.trim_end())?;
+
+    // Generate a public key based on the decoded private key, and encode it
+    // as base64
+    let public_key = curve25519::generate_public(&private_key)?;
+    let public_key = Base64::encode_string(&public_key);
+
+    Ok(public_key)
+}
+
 // Reads a base64 encoded private key from stdin and attempts to get a public
 // key from it.
-pub fn pubkey() -> Result<()> {
+pub fn pubkey() -> Result<String> {
     let mut buffer = String::new();
     let stdin = io::stdin();
     stdin.read_line(&mut buffer)?;
@@ -22,15 +35,25 @@ pub fn pubkey() -> Result<()> {
         return Err(anyhow!("Key is not the correct length or format"));
     }
 
-    // Get the private key base64 and decode it
-    let private_key = Base64::decode_vec(buffer.trim_end())?;
-
-    // Generate a public key based on the decoded private key, and encode it
-    // as base64
-    let public_key = curve25519::generate_public(&private_key)?;
-    let public_key = Base64::encode_string(&public_key);
+    let public_key = generate_public_key(&buffer)?;
 
     println!("{}", public_key);
 
-    Ok(())
+    Ok(public_key)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_generate_public_key_from_base64() {
+        // Taken from `wg` output.
+        let input    = "oOanTdwuPvcT0sRoLeJVDVKlIq9SQYsd8fKXC025gX0=";
+        let expected = "1Nae12IQvpY6eZ6Wa9RjJ+ytyCrZruj6nLNIcjbuoXk=";
+
+        let output = generate_public_key(&input).unwrap();
+
+        assert_eq!(output, expected);
+    }
 }
